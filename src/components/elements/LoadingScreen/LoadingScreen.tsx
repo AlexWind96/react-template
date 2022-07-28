084eb4b21f491ff2a01d441eaffe847a00a5c703
Alex Pochebut <alexpochebut96@gmail.com>
2022-07-29 00:14:51 +0300

    fix: add margin

--- src/components/elements/LoadingScreen/LoadingScreen.tsx
+++ src/components/elements/LoadingScreen/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import { Loader } from '@mantine/core'
 
 export const LoadingScreen = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center mt-4">
       <Loader size="lg" variant="dots" />
     </div>
   )
