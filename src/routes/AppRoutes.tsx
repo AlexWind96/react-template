7591d1852f81c542b436701f41c735966232d96c
Alex Pochebut <alexpochebut96@gmail.com>
2022-08-02 00:04:41 +0300

    refactor: routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,7 +6,5 @@ import { getAllRoutes } from './routes-configs'
 export const AppRoutes = () => {
   const { isLoggedIn, user } = useAuth()
 
-  const routes = useRoutes([...getAllRoutes(isLoggedIn, user)])
-
-  return <>{routes}</>
+  return useRoutes(getAllRoutes(isLoggedIn, user))
 }
